f7471cfd87476a5806d0c1470b3b616768912fa8
faiz <lovekh3549@gmail.com>
2022-01-19 15:26:42 +0900

    ✨Update

--- src/main/kotlin/io/github/faiz/Main.kt
+++ src/main/kotlin/io/github/faiz/Main.kt
@@ -21,9 +21,12 @@ class Main:JavaPlugin(),Listener {
     @EventHandler
     fun onInterraction(e:PlayerInteractEvent){
         val p = e.player
-        if (e.action == Action.RIGHT_CLICK_AIR || e.action == Action.RIGHT_CLICK_BLOCK){
-            if (p.itemOnCursor == ItemStack(Material.COBBLESTONE,20)){
-                p.inventory.remove(ItemStack(Material.COBBLESTONE,1))
+        val material: Material = p.inventory.itemInMainHand.type
+        val item = p.inventory.itemInMainHand
+        if (e.action == Action.LEFT_CLICK_AIR || e.action == Action.LEFT_CLICK_BLOCK && material == Material.COBBLESTONE){
+            if(p.isSneaking){
+                if (item.amount >= 20){
+                    item.amount = item.amount-20
                 val r = rand(0,100)
                 if (r == 0){
                     p.inventory.addItem(ItemStack(Material.DIAMOND,1))
@@ -44,15 +47,16 @@ class Main:JavaPlugin(),Listener {
                     val x = p.location.x
                     val y = p.location.y
                     val z = p.location.z
-                    var tnt:TNTPrimed = p.world.spawnEntity(Location(x,y,z), EntityType.PRIMED_TNT)
+                    val tnt:TNTPrimed = p.world.spawnEntity(Location(p.world,x,y,z), EntityType.PRIMED_TNT) as TNTPrimed
+                    tnt.fuseTicks = 10
                     p.sendMessage("§l§b5%의 확률로 TNT가 나왔습니다!")
                 }else if (r in 17 .. 26){
-                    p.inventory.addItem(ItemStack(Material.COBBLESTONE),10)
+                    p.inventory.addItem(ItemStack(Material.COBBLESTONE,10))
                     p.sendMessage("§l§b10%의 확률로 조약돌 10개가 나왔습니다!")
                 }else{
                     p.sendMessage("§l§b75%의 확률로 꽝이 나왔습니다!")
                 }
-            }
+            }}
         }
     }
 
